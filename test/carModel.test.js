87c41d361c01f809261cb7b645abafdc817b9d79
OlegKorniychuk <kornoleg576@gmail.com>
2025-02-08 22:11:49 +0200

    fix: attempt to fix CI

--- test/carModel.test.js
+++ test/carModel.test.js
@@ -11,23 +11,23 @@ const mongoConnection = process.env.MONGO_CONNECTION;
 const mongoPassword = process.env.MONGO_PASSWORD;
 const dbConnection = mongoConnection.replace('PASSWORD', mongoPassword);
 
-before(async () => {
-  await mongoose.connect(dbConnection);
+before(async function () {
+  await mongoose.connect(dbConnection)
   await Car.deleteMany({});
 });
 
-after(async () => {
-  await Car.deleteMany({});
+after(async function () {
+  await Car.deleteMany({})
   await mongoose.disconnect();
 });
 
-describe('Car model tests', () => {
-  it('Should get 0 results', async () => {
+describe('Car model tests', function() {
+  it('Should get 0 results', async function () {
     results = await Car.find();
     results.length.should.be.equal(0);
   });
 
-  it('Should succesfully create a car', async () => {
+  it('Should succesfully create a car', async function () {
     const carData = {
       make: '2001 Toyota Corola',
       type: 'sedan',
@@ -41,7 +41,7 @@ describe('Car model tests', () => {
     results.length.should.be.equal(1);
   });
 
-  it('Should have 5 required fields', async () => {
+  it('Should have 5 required fields', async function() {
     const carData = {};
     let error;
     try {
@@ -58,7 +58,7 @@ describe('Car model tests', () => {
     expect(error.errors).to.have.property('make');
   });
 
-  it('Should only accept certain values for car type', async () => {
+  it('Should only accept certain values for car type', async function() {
     const carData = {
       make: '2001 Toyota Corola',
       type: 'type',
@@ -86,7 +86,7 @@ describe('Car model tests', () => {
     ]);
   });
 
-  it('Should check for minimal production year', async () => {
+  it('Should check for minimal production year', async function() {
     const carData = {
       make: '2001 Toyota Corola',
       type: 'sedan',
@@ -105,7 +105,7 @@ describe('Car model tests', () => {
     expect(error.errors).to.have.property('productionYear');
   });
 
-  it('Should check for maximal production year', async () => {
+  it('Should check for maximal production year', async function() {
     const carData = {
       make: '2001 Toyota Corola',
       type: 'sedan',
@@ -124,7 +124,7 @@ describe('Car model tests', () => {
     expect(error.errors).to.have.property('productionYear');
   });
 
-  it('Should receive virtual property', async () => {
+  it('Should receive virtual property', async function() {
     const results = await Car.find();
     expect(results[0]).to.have.property('rentPerDay');
     expect(results[0].rentPerDay).to.be.a('number');
