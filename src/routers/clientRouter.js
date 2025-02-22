65d6cd474a211e0dc0da2c3646c7b9f739a339ed
OlegKorniychuk <kornoleg576@gmail.com>
2025-02-22 22:18:40 +0200

    feat: add endpoint for retreiving current user

--- src/routers/clientRouter.js
+++ src/routers/clientRouter.js
@@ -8,6 +8,10 @@ const router = express.Router();
 router.use('/:clientId/rentals', clientRentalRouter);
 
 router
+  .route('/me')
+  .get(authController.protect, clientController.readCurrent);
+
+router
   .route('/:clientId')
   .get(authController.protect, clientController.readOne);
 
